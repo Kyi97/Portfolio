7409f5abab9687eb48bf76ea2590e69ab037d516
kyisinthant <kyi.sin.thant@fusionsol.com>
2023-08-10 15:23:33 +0630

    init

---
+++ src/pages/home_page.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex mb-4">
+        <div class="w-2/3 bg-gray-500 h-12">
+
+        </div>
+        <div class="w-2/3 bg-gray-400 h-12">
+
+        </div>
+    </div>
+</template>
